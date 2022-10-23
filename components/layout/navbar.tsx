6b7db5b0dd846a81d760da6c7caa560c216d4195
ansango <anibalsantosgo@gmail.com>
2022-10-23 15:22:45 +0200

    fix btn

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -31,7 +31,7 @@ export const NavBar = () => {
           <div className="navbar-start">
             {isMounted && asPath !== "/" && (
               <Link href="/">
-                <a className="link link-hover font-serif italic font-semibold text-lg hover:text-primary transition-all duration-300">
+                <a className="btn btn-link text-neutral normal-case font-serif italic font-semibold text-lg hover:text-primary transition-all duration-300">
                   ansango
                 </a>
               </Link>
